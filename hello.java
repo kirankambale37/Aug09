d713a736a42355ea66142c7b86c1d42ee9b5eb71
unknown <kamablekiran10@gmail.com>
2024-08-10 13:59:10 +0530

    manager work

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 public class hello{
     public static void main(String[] args){
         System.err.println("hello world");
+	System.out.println("this is manager work);
     }
 }
--- javafile.java
+++ javafile.java
@@ -1,2 +1,9 @@
 hi its java file
 this 2nd line of code
+<<<<<<< Updated upstream
+this is manager work is done 
+
+this 1st line of code
+this 3rd line of code
+thi is 4th line of code
+>>>>>>> Stashed changes
