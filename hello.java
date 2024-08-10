020f9fd337c6fe1d60d6f67ffac0b920e566579b
unknown <kamablekiran10@gmail.com>
2024-08-10 14:01:43 +0530

    javba

--- hello.java
+++ hello.java
@@ -2,5 +2,8 @@ public class hello{
     public static void main(String[] args){
         System.err.println("hello world");
 	System.out.println("this is manager work);
+
+
+	System.out.println("this is my work);
     }
 }
